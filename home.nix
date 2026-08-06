3187bdb27134ced40df5085419d66f3d4ede5d2e
NewonetOrginal <jas.tryka@gmail.com>
2026-08-06 21:55:27 +0200

    test and change in home.nix

--- home.nix
+++ home.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
-
+let
+  c = config.lib.stylix.colors.withHashtag;
+  raw = config.lib.stylix.colors;
+in
 {
   home.username = "nixbtw";
   home.homeDirectory = "/home/nixbtw";
@@ -236,28 +239,26 @@
       font_family = "DejaVuSansM Nerd Font";
       background_opacity = "0.6";
 
-      foreground = "#eadcf5";
-      background = "#131218";
+      foreground = c.base05;
+      background = c.base00;
 
-      # Accents
-      color0 = "#000000";
-      color1 = "#df4661";
-      color2 = "#54d67f";
-      color3 = "#eea243";
-      color4 = "#3b68e5";
-      color5 = "#af70e8";
-      color6 = "#5ccfe6";
-      color7 = "#edeaff";
+      color0 = c.base00;
+      color1 = c.base08;
+      color2 = c.base0B;
+      color3 = c.base0A;
+      color4 = c.base0D;
+      color5 = c.base0E;
+      color6 = c.base0C;
+      color7 = c.base05;
 
-      # Bright Variants
-      color8 = "#3f3a47";
-      color9 = "#f76881";
-      color10 = "#7fff9f";
-      color11 = "#ffc475";
-      color12 = "#6085f2";
-      color13 = "#c694f5";
-      color14 = "#8ae4f2";
-      color15 = "#ffffff";
+      color8 = c.base03;
+      color9 = c.base08;
+      color10 = c.base0B;
+      color11 = c.base0A;
+      color12 = c.base0D;
+      color13 = c.base0E;
+      color14 = c.base0C;
+      color15 = c.base07;
     };
   };
 
