fd56d468a724f620515829da10e3218c3a06c48a
NewonetOrginal <jas.tryka@gmail.com>
2026-08-06 13:33:09 +0200

    Added Home Manager

--- flake.nix
+++ flake.nix
@@ -4,18 +4,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, spicetify-nix, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, spicetify-nix, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; }; # Passes inputs into configuration.nix
+      specialArgs = { inherit inputs; }; 
       modules = [
         spicetify-nix.nixosModules.default
+        home-manager.nixosModules.home-manager
         ./configuration.nix
       ];
     };
---
+++ home.nix
@@ -0,0 +1,64 @@
+{ pkgs, ... }:
+
+{
+  home.username = "nixbtw";
+  home.homeDirectory = "/home/nixbtw";
+  home.stateVersion = "24.05";
+
+  # Home Manager manages programs directly when possible
+  programs.home-manager.enable = true;
+
+  # Web Browsing
+  programs.firefox.enable = true;
+
+  # Shell & Terminal
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
+  };
+
+  programs.starship.enable = true;
+
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.git.enable = true;
+
+  # --- USER PACKAGES ---
+  # Applications, CLI tools, and desktop utilities go here.
+  home.packages = with pkgs; [
+    # Modern CLI Replacements
+    zoxide
+    bat
+    eza
+    fd
+    ripgrep
+    dust
+
+    # Terminal Applications
+    yazi
+    btop
+    fastfetch
+    helix
+    cava
+    pastel
+    rust-analyzer
+    opencode
+
+    # Desktop & Wayland Setup
+    waybar
+    rofi
+    awww
+    kitty
+
+    # User Applications & Launchers
+    filezilla
+    prismlauncher
+    beammp-launcher
+    playerctl
+  ];
+}
